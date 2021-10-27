5a49bb6678c703e948c5ee52d5097a7e561c23b3
Talita-S <talitassantos2015@gmail.com>
2021-10-27 16:27:41 -0300

    Post - findById

---
+++ src/main/java/com/fatec/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.fatec.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.workshopmongo.domain.Post;
+import com.fatec.workshopmongo.repository.PostRepository;
+import com.fatec.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
