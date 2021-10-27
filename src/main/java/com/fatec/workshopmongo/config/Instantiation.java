a9713a8b11e10f3d0529c60594b70be2c4a80422
Talita-S <talitassantos2015@gmail.com>
2021-10-27 15:45:51 -0300

    Entity post with nested user

--- src/main/java/com/fatec/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fatec/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.fatec.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.fatec.workshopmongo.domain.Post;
 import com.fatec.workshopmongo.domain.User;
+import com.fatec.workshopmongo.repository.PostRepository;
 import com.fatec.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,27 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		userRepository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		User u1 = new User(null, "Lynnete Scavo", "lynnete@gmail.com");
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
+		User u1 = new User(null, "Raymond Holt", "raymond@gmail.com");
 		User u2 = new User(null, "Rosa Diaz", "rosa@gmail.com");
 		User u3 = new User(null, "Jake Peralta", "jake@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("19/03/2021"), "I've already setup my Twitter account", "You can find me on @5261796d6e6420", u1);
+		Post post2 = new Post(null, sdf.parse("21/10/2021"), "I am retiring from the NYPD", "I need to spend more time with my husband and Cheddar", u1);
+		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
 }
